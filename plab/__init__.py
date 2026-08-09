1bdc0e900c55218eeed68071d63d7e10123fd4d1
Agustin Buruchaga <aburuchaga6@gmail.com>
2026-08-09 16:50:47 -0400

    Creacion comando

---
+++ plab/__init__.py
@@ -0,0 +1,3 @@
+"""Command-line launcher for PulsarLab."""
+
+__version__ = "0.1.0"
---
+++ plab/_streamlit_entry.py
@@ -0,0 +1,7 @@
+"""Streamlit entry point used by the `plab` command."""
+
+from app.main import run
+
+
+if __name__ == "__main__":
+    run()
