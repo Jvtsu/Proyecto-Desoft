e584665c1802b3a7a5fd4aeb822ac655ef1c5976
Agustin Buruchaga <aburuchaga6@gmail.com>
2026-08-14 18:59:15 -0400

    Delete plab directory

--- plab/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Command-line launcher for PulsarLab."""
-
-__version__ = "0.1.0"
--- plab/_streamlit_entry.py
+++
@@ -1,7 +0,0 @@
-"""Streamlit entry point used by the `plab` command."""
-
-from app.main import run
-
-
-if __name__ == "__main__":
-    run()
